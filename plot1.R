dc0804a535e9049f419f54a9312371c906dd5fca
Pradip Paneru <pradeeppaneru2@gmail.com>
2020-09-22 15:30:19 +0545

    assignment

---
+++ plot1.R
@@ -0,0 +1,1 @@
+hist(pl$Global_active_power, main="Global Active Power", xlab = "Global Active Power (kilowatts)", col="red")
---
+++ plot2.R
@@ -0,0 +1,1 @@
+plot(pl$Global_active_power~pl$dateTime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
